e913714dc15397f9cddf58a8b6910f89879cf20c
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-04-28 00:16:07 +0200

    a square has a name (Arya ? Is that you ?), fixes #6

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,11 +1,20 @@
-public class Square {
+public class Square
+{
     private String squareName;
 
+    /**
+     * Constructor
+     * @param newSquareName
+     */
     public Square(String newSquareName)
     {
         squareName = newSquareName;
     }
 
+    /**
+     * Getter
+     * @return name of the current square
+     */
     public String getSquareName()
     {
         return squareName;
