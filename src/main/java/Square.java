d02ab34cb9298059944b79ac21c3287c584c18c0
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-04-28 00:10:50 +0200

    test nom square, issue #6

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,3 +1,13 @@
 public class Square {
     private String squareName;
+
+    public Square(String newSquareName)
+    {
+        squareName = newSquareName;
+    }
+
+    public String getSquareName()
+    {
+        return squareName;
+    }
 }
