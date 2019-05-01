c33520f9226b67625488be59ea900cbf8f27a8bf
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-01 08:47:09 +0200

    ajout des méthodes de la classe Die, issue #11

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,3 +1,18 @@
 public class Die {
     private int faceValue;
+
+    public Die()
+    {
+        faceValue = 6;
+
+    }
+
+    public Die(int newFaceValue)
+    {
+        faceValue = newFaceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
 }
