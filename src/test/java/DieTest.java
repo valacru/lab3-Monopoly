f382b586ec3993f345b9fac4a36bc850cf4d9913
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-01 08:43:45 +0200

    réalisation test classe Die, issue #11

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DieTest {
+
+    @Test
+    void aDieShouldntHaveMoreThanSixFaces()
+    {
+        Die die = new Die();
+        assertEquals(die.getFaceValue(), 6);
+    }
 
 }
