df075c515f3665f01241612a67a7787d4409d47c
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-01 09:42:57 +0200

    Correction des tests (qui ne voulaient pas dire grand chose..., issue #11

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -5,10 +5,40 @@ import static org.junit.jupiter.api.Assertions.*;
 class DieTest {
 
     @Test
-    void aDieShouldntHaveMoreThanSixFaces()
+    void valueOfAFaceShouldBeInTheRightBoundaries()
     {
         Die die = new Die();
-        assertEquals(die.getFaceValue(), 6);
+        assertTrue((die.getFaceValue() <= 6) && (die.getFaceValue() >= 1) );
+    }
+
+    @Test
+    void everyValueShouldAppearAtLeastOneTimeOver100turns()
+    {
+        boolean one = false, two = false, three = false, four = false,
+                five = false, six = false;
+        for(int i = 0; i < 100000; ++i)
+        {
+            Die die = new Die();
+            int valeur = die.getFaceValue();
+            switch(valeur)
+            {
+                case 1 : one = true;
+                    break;
+                case 2 : two = true;
+                    break;
+                case 3 : three = true;
+                    break;
+                case 4 : four = true;
+                    break;
+                case 5 : five = true;
+                    break;
+                case 6 : six = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        assertTrue(one && two && three && four && five && six);
     }
 
 }
