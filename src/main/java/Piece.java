cf1bf257561edcdb8af4fe04b03d87fc45db7583
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-02 13:39:00 +0200

    ajout des attributs + méthodes de la classe Square, issue #9

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,3 +1,28 @@
 public class Piece {
     private String pieceName;
+    private Square location;
+    private String[] piecesNames = {"T-Rex", "Spinosaur", "Diplodocus", "Stegosaur", "Pteranodon",
+            "Utahraptor", "Carnotaur", "Triceratops"};
+    private static int counter = 0;
+
+    public Piece() {
+        pieceName = piecesNames[counter];
+        location = new Square();
+        if (counter >= 8) {
+            counter = 0;
+        } else {
+            ++counter;
+        }
+    }
+
+    public Square getLocation(Piece piece)
+    {
+        return piece.location;
+    }
+
+    public String getPieceName()
+    {
+        return pieceName;
+    }
+
 }
