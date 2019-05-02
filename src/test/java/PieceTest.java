0675f52b2b374900f95750f64617d3bc1d7b61c9
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-02 13:35:41 +0200

    création 1er test de la classe piece, issue #9

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PieceTest {
 
+    @Test
+    void everyPieceOfAGameShouldBeDifferent()
+    {
+        Piece piece1 = new Piece();
+        Piece piece2 = new Piece();
+
+        assertNotEquals(piece1.getPieceName(), piece2.getPieceName());
+    }
 }
