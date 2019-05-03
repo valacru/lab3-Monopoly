3d152ba200fcc9679012dfcd17c4d559f42f94dd
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-04 00:57:29 +0200

    mise à jour test fb-Square, fixes #16

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquareTest {
 
     @Test
-    void aSquareShouldHaveAName()
+    void sequentialSquareShouldHaveTheCorrectName()
     {
-        Square squareToTest = new Square();
-        String nameToTest = "Go";
+        Square s1 = new Square();
+        Square s2 = new Square();
+        Square s3 = new Square();
 
-        assertEquals(squareToTest.getSquareName(), nameToTest);
+        assertEquals(s1.getSquareName(), "Go");
+        assertEquals(s2.getSquareName(), "Square 1");
+        assertEquals(s3.getSquareName(), "Square 2");
+
     }
 }
