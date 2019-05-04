b3b90ed989444773ff41bfb20256934e081490b6
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-04 02:41:14 +0200

    ajout 1er test classe Player, issue #15

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,32 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+
+    @Test
+    void playerShouldHaveTheCorrectName()
+    {
+        String lambdaName = "Elodie";
+        Player player = new Player(lambdaName);
+        assertEquals(player.getPlayerName(), lambdaName);
+    }
+
+    @Test
+    void playerShouldHaveAnExistingPiece()
+    {
+        int index = 0;
+        String pieceNameToTest = null;
+        Player player = new Player("Guris");
+        for(; index < 8; ++index)
+        {
+            if(player.getPiece().getPieceName().equals(player.getPiece().getPiecesName(index)))
+            {
+                pieceNameToTest = player.getPiece().getPiecesName(index);
+                break;
+            }
+        }
+        assertEquals(player.getPiece().getPieceName(), pieceNameToTest);
+    }
 
 }
