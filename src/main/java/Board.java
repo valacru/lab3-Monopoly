e6694701236bb04e831b51735a0b3d0c9d681e00
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-04 02:01:43 +0200

    ajout méthodes + attributs de la classe Board, fixes #10

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Board {
+    private ArrayList<Square> squares;
+    private static final int NB_SQUARES = 40;
+
+    public Board()
+    {
+        squares = new ArrayList<Square>();
+        for(int i = 0; i <= 39; ++i)
+        {
+            if(i == 0)
+            {
+                squares.add(new Square("Go"));
+            }
+            else
+            {
+                squares.add(new Square("Square" + i));
+            }
+        }
+
+    }
+
+    public Square getSquare(String oldLoc, int fvTot) {
+        int i = 0;
+        for (; i < NB_SQUARES; ++i) {
+            if (squares.get(i).getSquareName().equals(oldLoc)) {
+                break;
+            }
+            ++i;
+        }
+        return squares.get((i + fvTot) % NB_SQUARES);
+    }
 }
