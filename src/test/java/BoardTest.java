8de52d9227a152b7a4afca3fcd3e142080cd47c6
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-04 02:00:08 +0200

    ajoute 1er test class Board, issue #10

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    @Test
+    void squareShouldBeCorrectlyGeneratedOnBoard()
+    {
+        Board board = new Board();
+
+        assertEquals(board.getSquare(null, 0).getSquareName(), "Go");
+    }
 
 }
