dbbc7e414eaf424770378f05416d2e8f57ca93ad
CosmicElodie <CosmicElodie@users.noreply.github.com>
2019-05-05 22:15:01 +0200

    rendu final

--- src/test/java/MonopolyTests/BoardTests/BoardTest.java
+++ src/test/java/MonopolyTests/BoardTests/BoardTest.java
@@ -14,4 +14,20 @@ class BoardTest {
         assertEquals(board.getSquare(null, 0).getSquareName(), "Go");
     }
 
+    @Test
+    void aBoardShouldHaveASize()
+    {
+        Board board1 = new Board();
+        int taille1 = board1.getSquaresSize();
+        assertNotNull(taille1);
+    }
+
+    @Test
+    void aBoardInitializedWithAnotherBoardShouldBeTheSame()
+    {
+        Board b1 = new Board();
+        Board b2 = b1;
+        assertIterableEquals(b1.getArraySquares(), b2.getArraySquares());
+    }
+
 }
